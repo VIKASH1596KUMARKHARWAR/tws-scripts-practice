8a647e5bbb913f24c1d2bdb6b76261b325bb832c
Ubuntu <ubuntu@ip-172-31-41-208.eu-north-1.compute.internal>
2025-03-10 17:16:40 +0000

    init-c0mmit

---
+++ vikash.sh
@@ -0,0 +1,27 @@
+#jai bajrang bali
+#!/bin/bash
+
+#user Defined variables
+hero="rancho"
+villian="virus"
+
+echo "3-idot ka hereo hai $hero"
+echo "3 idot ka villain hain $villian"
+
+echo "chatur :Adarniya sabhapati mahodya aaj hum sb mil ke script padhenge "
+
+echo "rancho : ratta nhi marne ka "
+
+echo "ek folder banate h"
+mkdir -p rancho
+
+echo "farnhan: virus ne mizzati fr de bola farhan ntrate "
+
+touch farhanNitrate.txt
+
+echo "abba nhi mannege" >farhanNitrate.txt
+
+echo "aab ek fiel file1.txt bana and put it in ranco"
+
+touch file1.txt 
+mv file1.txt rancho/
